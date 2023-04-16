be2805ab52c3b6b7cbb59e32a3cdc2fd6451e50c
Stuart <stuart295@gmail.com>
2023-04-16 21:52:56 +0200

    Some cleanup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ChatGPT-4-API-Interface
-A super-basic flask app that provides an ChatGPT-like interface to the OpenAI API
+## Introduction
+A super-basic flask app that provides an ChatGPT-like interface to the OpenAI API. 
+
+If you have access to the GPT4 APIs, then this app allows you to use the GPT4 model in a ChatGPT-like way without needing ChatGPT plus. As a bonus, there are no message limits for the API and you are billed based on your actual usage.
+
+## Installation
+To use this:
+1. Install the dependencies: `pip install -r requirements.txt`
+2. Create a `secrets.txt` file in the root directory containing an active OpenAI API key. Not great, I know.
+3. Run the flask app: `python app.py`
--- app.py
+++ app.py
@@ -1,58 +1,48 @@
-from pathlib import Path
 import openai
-
+from pathlib import Path
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 SECRETS_FILE = 'secrets.txt'
-secrets_file_path = Path(SECRETS_FILE)
 
-if not secrets_file_path.exists():
-    raise FileNotFoundError(f"Secrets file '{SECRETS_FILE}' not found.")
+def load_api_key(file_path: Path) -> str:
+    if not file_path.exists():
+        raise FileNotFoundError(f"Secrets file '{file_path}' not found.")
 
-with open(SECRETS_FILE, 'r') as f:
-    API_KEY = f.read().strip()
+    with file_path.open('r') as f:
+        api_key = f.read().strip()
 
-if not API_KEY:
-    raise ValueError("API key not found in secrets file.")
+    if not api_key:
+        raise ValueError("API key not found in secrets file.")
 
-# Set the OpenAI API key
-openai.api_key = API_KEY
+    return api_key
 
+openai.api_key = load_api_key(Path(SECRETS_FILE))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 def ask_gpt4(messages):
     data = {
-        'model': 'gpt-4',  # Update this with the correct model name when GPT-4 is available
+        'model': 'gpt-4',
         'messages': messages
     }
-    response = openai.ChatCompletion.create(**data)
-    return response
-
-conversation = [{'role': 'system', 'content': 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.'}]
-
+    return openai.ChatCompletion.create(**data)
 
 @app.route('/ask', methods=['POST'])
 def ask():
     user_input = request.form['user_input']
     system_message = request.form['system_message']
-    user_message = {'role': 'user', 'content': user_input}
-
-    conversation = [{'role': 'system', 'content': system_message}]
-    conversation.append(user_message)
+    conversation = [
+        {'role': 'system', 'content': system_message},
+        {'role': 'user', 'content': user_input}
+    ]
 
     response = ask_gpt4(conversation)
     gpt4_response = response['choices'][0]['message']['content'].strip()
-    assistant_message = {'role': 'assistant', 'content': gpt4_response}
-    conversation.append(assistant_message)
-
     return jsonify({'response': gpt4_response})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
